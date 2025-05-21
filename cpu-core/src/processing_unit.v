12fa26545030077a579bdd8c5781ebaddb3c69bc
Tanmay <doubleimpala.17@gmail.com>
2025-05-21 15:40:00 -0400

    Began ALU without pipelining.

---
+++ cpu-core/src/processing_unit.v
@@ -0,0 +1,9 @@
+module ALU(input clk, input [2:0] func, input [31:0] operand_1, operand_2, output [31:0] result);
+    // Change to always for pipelining.
+    assign result = (func == 000)? operand_1 + operand_2 :
+                    (func == 001)? operand_1 - operand_2 :
+                    (func == 010)? operand_1 & operand_2 :
+                    (func == 011)? operand_1 | operand_2 :
+                    3s'b0;
+                    // Oppurtunity for added functionality.
+endmodule
