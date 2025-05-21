ea657636f7aec22b81cddd3c74274c8423b20b5c
tanmay <doubleimpala.17@gmail.com>
2025-05-21 18:26:41 -0400

    Fixed ALU bug.

--- cpu-core/src/processing_unit.v
+++ cpu-core/src/processing_unit.v
@@ -4,6 +4,6 @@ module ALU(input clk, input [2:0] func, input [31:0] operand_1, operand_2, outpu
                     (func == 001)? operand_1 - operand_2 :
                     (func == 010)? operand_1 & operand_2 :
                     (func == 011)? operand_1 | operand_2 :
-                    3s'b0;
+                    3'b0;
                     // Oppurtunity for added functionality.
 endmodule
