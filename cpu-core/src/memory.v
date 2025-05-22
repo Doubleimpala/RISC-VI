a72371dbd4bee82a93944deabc4d88be6d5e4c4b
tanmay <doubleimpala.17@gmail.com>
2025-05-21 23:57:27 -0400

    Memory skeleton, added cache file.

---
+++ cpu-core/src/memory.v
@@ -0,0 +1,11 @@
+module MAR(input clk, input ld, wr, rst, input [31:0] PC, output [31:0] address);
+
+endmodule 
+
+module memory(input [31:0] address, output [31:0] data)
+
+endmodule
+
+module MDR(input clk, input ld, wr, rst, input [31:0] data_in, output [31:0] data_out);
+
+endmodule 
