d28918103748f838d63cb8a75fc0a5ae705a972d
tanmay <doubleimpala.17@gmail.com>
2025-07-31 11:59:46 -0400

    Added shift functionality to ALU.

--- cpu-core/src/processing_unit.sv
+++ cpu-core/src/processing_unit.sv
@@ -1,9 +1,12 @@
 module ALU(input clk, input [2:0] func, input [31:0] operand_1, operand_2, output [31:0] result);
     // Change to always for pipelining.
-    assign result = (func == 000)? operand_1 + operand_2 :
-                    (func == 001)? operand_1 - operand_2 :
-                    (func == 010)? operand_1 & operand_2 :
-                    (func == 011)? operand_1 | operand_2 :
+    assign result = (func == 000)? operand_1 + operand_2 : // Add
+                    (func == 001)? operand_1 - operand_2 : // Subtract
+                    (func == 010)? operand_1 & operand_2 : // AND
+                    (func == 011)? operand_1 | operand_2 : // OR
+                    (func == 100)? operand_1 << operand_2[4:0] : // Shift left
+                    (func == 101)? operand_1 >> operand_2[4:0] : // Shift right
+                    (func == 110)? $signed(operand_1) >>> operand_2[4:0] : // Arithmetic shift right
+                    // 111 is unused.
                     32'b0;
-                    // Oppurtunity for added functionality.
 endmodule
